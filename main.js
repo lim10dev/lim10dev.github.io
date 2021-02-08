6c9a3502e1df0aa2b880cade521b44ddec169c7d
lim10dev <60268293+lim10dev@users.noreply.github.com>
2021-02-08 23:48:36 +0300

    Little style changes

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 { // Setup
+console.log("Setup start...");
+var msg = document.getElementById("msg");
+msg.innerHTML = "";
 var EnableMods = document.getElementById("EnableMods");
 var SilenceSounds = document.getElementById("SilenceSounds");
 var Task_CanAttackMouse = document.getElementById("Task_CanAttackMouse");
@@ -27,8 +30,10 @@ config.value = config_text;
 } // Setup End
 
 addEventListener("load", function(){
+    console.log("Load Start...\nGetting URL Parameters...");
     const urlParams = new URLSearchParams(window.location.search);
-    if(!(urlParams == "")){
+    if(urlParams != ""){
+        console.log("URL Parameters exist. Loading page content...");
         var nms = ["EnableMods", "SilenceSounds", "Task_CanAttackMouse", "AttackRandomly", "UseCustomColors", "GooseDefaultWhite", "GooseDefaultOrange", 
         "GooseDefaultOutline", "MaxWanderingTimeSeconds", "MinWanderingTimeSeconds", "FirstWanderTimeSeconds"];
         var txt = "";
@@ -96,6 +101,7 @@ addEventListener("load", function(){
                             FirstWanderTimeSeconds.value = urlParams.get(nms[i]);
                             break;
                     }
+                    msg.innerHTML = 'Settings loaded sucessfully.';
                     if(i >= 5 && i <= 7){
                         txt += nms[i] + "=#" + urlParams.get(nms[i]) + "\n";
                     } else {
@@ -113,6 +119,7 @@ addEventListener("load", function(){
         }
     }
     apply.click();
+    msg.innerHTML = '';
     document.activeElement.blur()
 })
 
@@ -121,8 +128,13 @@ share.addEventListener("click", function() {
         apply.click();
         var i =  `https://lim10dev.github.io/?EnableMods=${capitalizeFirstLetter( EnableMods.checked.toString() )}&SilenceSounds=${capitalizeFirstLetter(SilenceSounds.checked.toString())}&Task_CanAttackMouse=${capitalizeFirstLetter(Task_CanAttackMouse.checked.toString())}&AttackRandomly=${capitalizeFirstLetter(AttackRandomly.checked.toString())}&UseCustomColors=${capitalizeFirstLetter(UseCustomColors.checked.toString())}&GooseDefaultWhite=${GooseDefaultWhite.value.slice(1)}&GooseDefaultOrange=${GooseDefaultOrange.value.slice(1)}&GooseDefaultOutline=${GooseDefaultOutline.value.slice(1)}&MinWanderingTimeSeconds=${MinWanderingTimeSeconds.value}&MaxWanderingTimeSeconds=${MaxWanderingTimeSeconds.value}&FirstWanderTimeSeconds=${FirstWanderTimeSeconds.value}`;
         navigator.clipboard.writeText(i);
+        console.log(`URL copied to clipboard:"${i}"`);
     }
     share.focus();
+    msg.innerHTML = 'Link copied to clipboard.';
+    setTimeout(function(){
+        msg.innerHTML = '';
+    }, 3000);
 });
 
 // Function to capitalize first letter of a word
@@ -132,25 +144,46 @@ function capitalizeFirstLetter(str) {
   
 document.onkeyup = function(e){
         if(e.keyCode == 13)
-        apply.click();  
+        apply.click();
 };
 
 save.addEventListener("click", function(){
+    console.log("Saving settings...");
     localStorage.setItem("settings", `?EnableMods=${capitalizeFirstLetter( EnableMods.checked.toString() )}&SilenceSounds=${capitalizeFirstLetter(SilenceSounds.checked.toString())}&Task_CanAttackMouse=${capitalizeFirstLetter(Task_CanAttackMouse.checked.toString())}&AttackRandomly=${capitalizeFirstLetter(AttackRandomly.checked.toString())}&UseCustomColors=${capitalizeFirstLetter(UseCustomColors.checked.toString())}&GooseDefaultWhite=${GooseDefaultWhite.value.slice(1)}&GooseDefaultOrange=${GooseDefaultOrange.value.slice(1)}&GooseDefaultOutline=${GooseDefaultOutline.value.slice(1)}&MinWanderingTimeSeconds=${MinWanderingTimeSeconds.value}&MaxWanderingTimeSeconds=${MaxWanderingTimeSeconds.value}&FirstWanderTimeSeconds=${FirstWanderTimeSeconds.value}`);
-    alert("Your settings are saved sucesfully in browser local storage.");
+    msg.innerHTML = 'Settings saved to your Browser Local Storage.';
+    setTimeout(function(){
+        msg.innerHTML = '';
+    }, 3000);
 });
 
 load.addEventListener("click", function(){
+    console.log("Loading settings...");
     if(localStorage.getItem("settings") != null){
+        console.log("Redirecting...");
         document.location.href = "https://lim10dev.github.io/" + localStorage.getItem("settings");
     }
 });
 
 // when apply click
 apply.addEventListener("click", function() {
-    var nllchk = ""+GooseDefaultWhite.value + GooseDefaultOrange.value + GooseDefaultOutline.value + MaxWanderingTimeSeconds.value + MinWanderingTimeSeconds.value + FirstWanderTimeSeconds.value;
-    if(nllchk.includes("null")){
-        console.error("Invalid Input");
+    console.log('Applying settings...');
+    function nllchk() {
+        if((GooseDefaultWhite.value + GooseDefaultOrange.value + GooseDefaultOutline.value + MaxWanderingTimeSeconds.value + MinWanderingTimeSeconds.value + FirstWanderTimeSeconds.value).includes("null"))
+        return true;
+
+        var nms = ["EnableMods", "SilenceSounds", "Task_CanAttackMouse", "AttackRandomly", "UseCustomColors", "GooseDefaultWhite", "GooseDefaultOrange", 
+        "GooseDefaultOutline", "MaxWanderingTimeSeconds", "MinWanderingTimeSeconds", "FirstWanderTimeSeconds"];
+        for(i = 0; i < nms.length; i++){
+            if(document.getElementById(nms[i]).value == '' || document.getElementById(nms[i]).value == null){
+                return true;
+            }
+        }
+        return false;
+    }
+    console.log("Checking values...");
+    if(nllchk()){
+        console.error("Invalid Input. Rewriting values...");
+        msg.innerHTML = '';
         EnableMods.checked = false;
         SilenceSounds.checked = false;
         Task_CanAttackMouse.checked = true;
@@ -159,19 +192,26 @@ apply.addEventListener("click", function() {
         GooseDefaultWhite.value = "#ffffff";
         GooseDefaultOrange.value = "#ffa500";
         GooseDefaultOutline.value = "#d3d3d3";
-        MaxWanderingTimeSeconds.value = 20;
-        MinWanderingTimeSeconds.value = 40;
+        MinWanderingTimeSeconds.value = 20;
+        MaxWanderingTimeSeconds.value = 40;
         FirstWanderTimeSeconds.value = 20;
     }
     config_text = `Version_DoNotEdit=1\nEnableMods=${capitalizeFirstLetter( EnableMods.checked.toString() )}\nSilenceSounds=${capitalizeFirstLetter(SilenceSounds.checked.toString())}\nTask_CanAttackMouse=${capitalizeFirstLetter(Task_CanAttackMouse.checked.toString())}\nAttackRandomly=${capitalizeFirstLetter(AttackRandomly.checked.toString())}\nUseCustomColors=${capitalizeFirstLetter(UseCustomColors.checked.toString())}\nGooseDefaultWhite=${GooseDefaultWhite.value}\nGooseDefaultOrange=${GooseDefaultOrange.value}\nGooseDefaultOutline=${GooseDefaultOutline.value}\nMinWanderingTimeSeconds=${MinWanderingTimeSeconds.value}\nMaxWanderingTimeSeconds=${MaxWanderingTimeSeconds.value}\nFirstWanderTimeSeconds=${FirstWanderTimeSeconds.value}`;
     config.value = config_text;
     config.focus();
+    console.log('Drawing Goose...');
     goose_body_svg[0].attributes["fill"].value = GooseDefaultWhite.value;
     goose_body_svg[0].attributes["stroke"].value = GooseDefaultOutline.value
     goose_beak_svg[0].attributes["fill"].value = goose_beak_svg[1].attributes["fill"].value = GooseDefaultOrange.value
+    msg.innerText = 'Settings applied.\nIf Goose\'s color preview doesn\'t match,\ntry turning Dark Mode Extensions off'
+    setTimeout(function(){
+        msg.innerHTML = '';
+    }, 7000);
 });
 // Spaghettiest code i've ever seen
 MaxWanderingTimeSeconds.addEventListener("change", function() {
+    if(MaxWanderingTimeSeconds.value.ToString().includes("e") || MaxWanderingTimeSeconds.value.ToString().includes("+") || MaxWanderingTimeSeconds.value.ToString().includes("-")){
+        MaxWanderingTimeSeconds.value = 0; }
     if (MaxWanderingTimeSeconds.value < MinWanderingTimeSeconds.value)
         MaxWanderingTimeSeconds.value = MinWanderingTimeSeconds.value;
     
@@ -182,6 +222,8 @@ MaxWanderingTimeSeconds.addEventListener("change", function() {
     }
 });
 MinWanderingTimeSeconds.addEventListener("change", function() {
+    if(MinWanderingTimeSeconds.value.ToString().includes("e") || MinWanderingTimeSeconds.value.ToString().includes("+") || MinWanderingTimeSeconds.value.ToString().includes("-")){
+    MinWanderingTimeSeconds.value = 0; }
     if (MinWanderingTimeSeconds.value > MaxWanderingTimeSeconds.value)
         MinWanderingTimeSeconds.value = MaxWanderingTimeSeconds.value;
     
@@ -192,6 +234,8 @@ MinWanderingTimeSeconds.addEventListener("change", function() {
         }
 } );
 FirstWanderTimeSeconds.addEventListener("change", function() {
+    if(FirstWanderTimeSeconds.value.ToString().includes("e") || FirstWanderTimeSeconds.value.ToString().includes("+") || FirstWanderTimeSeconds.value.ToString().includes("-")){
+        FirstWanderTimeSeconds.value = 0;}
     if (FirstWanderTimeSeconds.value < 1) {
         FirstWanderTimeSeconds.value = 0;
     } else if (FirstWanderTimeSeconds.value > 99999){
@@ -203,12 +247,14 @@ copy.addEventListener("click", function() {
     config.select();
     config.setSelectionRange(0, 99999);
     document.execCommand("copy");
+    console.log(`Settings Copied to clipboard: "${config.value}"`);
 });
 // Copy text
 
 down.addEventListener("click", function(){download(config_text, "config.ini", "plain/text")});
 // Function to download data to a file
 function download(data, filename, type) {
+    console.log("Started download of: " + filename);
     var file = new Blob([data], {type: type});
     if (window.navigator.msSaveOrOpenBlob) // IE10+
         window.navigator.msSaveOrOpenBlob(file, filename);
