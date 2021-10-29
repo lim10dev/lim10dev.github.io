f6859295d485ba8ad01f35c845c55e34a405d904
lim10dev <60268293+lim10dev@users.noreply.github.com>
2021-10-29 12:01:09 +0300

    Asal Çarpan Hesaplama Şeysisi
    Ah Canım Sevgilim (demo) güzel bi şarkı

--- ebob-ekok/main.js
+++ ebob-ekok/main.js
@@ -24,45 +24,86 @@ var ucuncusayi;
 
 var maxDeger = 4294967295; // (2^32)-1 olması için neden yok sadece havalı olsun diye öyle yazdım
 
+var kacKereGecer = (number, numbers) => numbers.reduce((counter, currentNumber) => (number === currentNumber ? counter + 1 : counter), 0);
+// https://stackoverflow.com/a/62722045/14033383
+
 document.onkeyup = function (ev) {
     if (ev.key == "Enter") hesapla.onclick();
 }
 
+/**
+ * 
+ * @param {Number} sayi Üs haline çevrilecek sayı.
+ * @return {String} Üs haline çevrilmiş yazı. 
+ */
+function usseCevir(sayi) {
+    var usler = ["⁰", "¹", "²", "³", "⁴", "⁵", "⁶", "⁷", "⁸", "⁹"];
+    var a = sayi.toString();
+    var usHali = "";
+    for (var j = 0; j < a.length; j++) {
+        usHali += usler[Number(a.charAt(j))];
+    }
+    return usHali;
+}
+
 hesapla.onclick = function () {
     var sonuc = 1; // Aşağıdaki çarpma işleminin çalışması için bu değişkene bir değer atamamız lazım.
-    birincisayi = Number(ilkSayiEl.value);
-    ikincisayi = Number(ikinciSayiEl.value);
-    ucuncusayi = (ucuncuSayiOlsunMu.checked ? Number(ucuncuSayiEl.value) : birincisayi);
-    var sadeceCarpanlar = [];
-    // Eğer üçüncü sayı olacak ise üçüncü sayıyı olduğu gibi alıyor. Eğer alınmayacak ise ilk sayıyı üçüncü sayı olarak yazıyor.
-    var carpanlar = carpanlarHesapla(birincisayi, ikincisayi, ucuncusayi)[Number(secenek.value == "ebob")];
-    // Çarpanlarını hesaplamak için fonksiyonumsu şey.
-    for (i = 0; i < carpanlar.length; i++) {
-        if (!sadeceCarpanlar.includes(carpanlar[i])) sadeceCarpanlar.push(carpanlar[i]);
-        sonuc *= carpanlar[i];
-    }
     sonucHeader.innerText = "Sonuç:";
-    sonucYazi.innerText = sonuc;
-    var txt = "Çarpanlar: "
-    const kacKereGecer = (number, numbers) => numbers.reduce((counter, currentNumber) => (number === currentNumber ? counter + 1 : counter), 0);
-    // https://stackoverflow.com/a/62722045/14033383
-    if (sadeceCarpanlar.length == 0) {
-        txt = "Asal çarpan yok."
-    } else {
-        for (i = 0; i < sadeceCarpanlar.length; i++) {
-            txt += `${sadeceCarpanlar[i]}(${kacKereGecer(carpanlar[i], carpanlar)})${(i + 1 == sadeceCarpanlar.length ? "" : ", ")}`
+    birincisayi = Number(ilkSayiEl.value);
+    var txt = "";
+    if (secenek.value == "asal") {
+        var carpanlar = asalCarpanlarHesapla(birincisayi);
+        var sadeceCarpanlar = [];
+        if (carpanlar.length == 0) {
+            txt = "Asal çarpan yok.";
+        } else {
+            for (var i = 0; i < carpanlar.length; i++) {
+                if (!sadeceCarpanlar.includes(carpanlar[i])) sadeceCarpanlar.push(carpanlar[i]);
+            }
+            for (var i = 0; i < sadeceCarpanlar.length; i++) {
+                txt += `${sadeceCarpanlar[i]}${usseCevir(kacKereGecer(sadeceCarpanlar[i], carpanlar))}${(i + 1 == sadeceCarpanlar.length ? "" : "×")}`;
+            }
         }
+        sonucYazi.innerText = txt;
+    } else {
+        ikincisayi = Number(ikinciSayiEl.value);
+        ucuncusayi = (ucuncuSayiOlsunMu.checked ? Number(ucuncuSayiEl.value) : birincisayi);
+        var sadeceCarpanlar = [];
+        // Eğer üçüncü sayı olacak ise üçüncü sayıyı olduğu gibi alıyor. Eğer alınmayacak ise ilk sayıyı üçüncü sayı olarak yazıyor.
+        var carpanlar = carpanlarHesapla(birincisayi, ikincisayi, ucuncusayi)[Number(secenek.value == "ebob")];
+        // Çarpanlarını hesaplamak için fonksiyonumsu şey.
+        for (var i = 0; i < carpanlar.length; i++) {
+            if (!sadeceCarpanlar.includes(carpanlar[i])) sadeceCarpanlar.push(carpanlar[i]);
+            sonuc *= carpanlar[i];
+        }
+        sonucYazi.innerText = sonuc;
+        var txt = "Çarpanlar: "
+        if (sadeceCarpanlar.length == 0) {
+            txt = "Asal çarpan yok."
+        } else {
+            for (var i = 0; i < sadeceCarpanlar.length; i++) {
+                txt += `${sadeceCarpanlar[i]}${usseCevir(kacKereGecer(carpanlar[i], carpanlar))}${(i + 1 == sadeceCarpanlar.length ? "" : "×")}`
+            }
+        }
+        carpanlarYazi.innerText = txt;
     }
-    carpanlarYazi.innerText = txt;
     sonucHeader.classList.add("sonucVar");
 }
 
 ucuncuSayiOlsunMu.onchange = function () {
-    ucuncuSayiEl.disabled = !ucuncuSayiOlsunMu.checked;
+    ucuncuSayiEl.disabled = (!ucuncuSayiOlsunMu.checked || secenek.value == "asal");
     normalDurumaGec();
 }
 
-secenek.onchange = normalDurumaGec;
+secenek.onchange = function () {
+    if (secenek.value == "asal") {
+        ikinciSayiEl.disabled = ucuncuSayiEl.disabled = true;
+    } else {
+        ikinciSayiEl.disabled = false;
+        ucuncuSayiEl.disabled = !ucuncuSayiOlsunMu.checked;
+    }
+    normalDurumaGec();
+}
 
 ilkSayiEl.onchange = function () {
     var a = ilkSayiEl.value;
@@ -87,9 +128,9 @@ ucuncuSayiEl.onchange = function () {
  * @return {Number[][]} 2 Küme: İlk küme tüm çarpanlar, İkinci küme ortak çarpanlar.
  */
 function carpanlarHesapla(sayi1, sayi2, sayi3) {
-    var a = (sayi1 < 1 ? 1 : sayi1);
-    var b = (sayi2 < 1 ? 1 : sayi2);
-    var c = (sayi3 < 1 ? 1 : sayi3);
+    var a = sayi1;
+    var b = sayi2;
+    var c = sayi3;
     var bolen = 2;
     var ortakCarpanlar = [];
     var carpanlar = [];
@@ -102,6 +143,23 @@ function carpanlarHesapla(sayi1, sayi2, sayi3) {
         if (!(a % bolen == 0 || b % bolen == 0 || c % bolen == 0)) bolen++;
     }
     return [carpanlar, ortakCarpanlar];
+}
+/**
+ *
+ * @param {Number} sayi1 Asal Çarpanları Hesaplanacak Sayı
+ * @return {Number[]}
+ */
+function asalCarpanlarHesapla(sayi1) {
+    var a = sayi1;
+    var bolen = 2;
+    var carpanlar = [];
+    while (a > 1) {
+        if (a % bolen == 0) {
+            a /= bolen;
+            carpanlar.push(bolen);
+        } else bolen++;
+    }
+    return carpanlar;
 }
 
 function normalDurumaGec() {
